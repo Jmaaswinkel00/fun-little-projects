93b8baa030b36ebd98954c8ee205d34426a1c35b
Jmaaswinkel00 <jmaaswinkel00@gmail.com>
2024-01-11 13:15:02 +0100

    added a decrypt function and made it all to their own class

---
+++ python/encryption/increaseLetterValue.py
@@ -0,0 +1,29 @@
+#This is a simple encryption which will just increase the ascii value of a letter
+class increaseLetterValue:
+    def __init__(self):
+        self.encryptedList = []
+        self.decryptedList = []
+        self.encryptedString = ""
+        self.decryptedString = ""
+        self.valueIncrease = 5
+        
+
+    def encrypt(self, wordToEncrypt: str):
+        listOfChars = list(wordToEncrypt)
+        for i, char in enumerate(listOfChars):
+            encryptedChar = ord(char)
+            if(encryptedChar+self.valueIncrease > 122):
+                encryptedChar = ord(char) - 26
+            self.encryptedList.append(chr(encryptedChar + self.valueIncrease))
+        
+        return self.encryptedString.join(self.encryptedList)
+    
+    def decrypt(self, wordToDecrypt: str):
+        listOfChars = list(wordToDecrypt)
+        for i, char in enumerate(listOfChars):
+            decryptedChar = ord(char)
+            if(decryptedChar-self.valueIncrease < 97):
+                decryptedChar = ord(char) + 26
+            self.decryptedList.append(chr(decryptedChar - self.valueIncrease))
+            
+        return self.decryptedString.join(self.decryptedList)
--- python/encryption/main.py
+++ python/encryption/main.py
@@ -1,21 +1,12 @@
+from increaseLetterValue import increaseLetterValue
 
 
 wordToEncrypt = input("Please give the word you want to encrypt: ")
-encryptedList = []
-encryptedString = ""
+increaseLetterValue = increaseLetterValue()
 
-#This is a simple encryption which will just increase the ascii value of a letter
-def increaseLetterValue(wordToEncrypt: str):
-    listOfChars = list(wordToEncrypt)
-    for i, char in enumerate(listOfChars):
-        encryptedChar = ord(char)
-        if(encryptedChar+5 > 122):
-            encryptedChar = ord(char) - 26
-        encryptedList.append(chr(encryptedChar + 5))
-    
-    return encryptedString.join(encryptedList)
+def main():
+    print(f"our encrypted string is: {increaseLetterValue.encrypt(wordToEncrypt)}")
+    print(f"our decrypted string is: {increaseLetterValue.decrypt('yjxy')}")
 
-
-
-
-print(f"our encrypted string is: {increaseLetterValue(wordToEncrypt)}")
+if __name__ == "__main__":
+    main()
